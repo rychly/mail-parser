78bbac202be654e78758988c2fd4b7473d1f02c8
Marek Rychly <marek.rychly@gmail.com>
2019-06-06 23:35:37 +0200

    Parsing input is not an iterator through lines, not an array of the lines, so a message file can be parsed during reading.

--- mail_parser.lua
+++ mail_parser.lua
@@ -5,162 +5,38 @@ local lower, find, sub, gsub = string.lower, string.find, string.sub, string.gsu
 local stdin, stdout, stderr, open, close, read, write = io.stdin, io.stdout, io.stderr, io.open, io.close, io.read, io.write
 
 -- Module declaration
-local mail_parser = {} -- Public namespace
+local _M = {}
 
--- PUBLIC FUNCTIONS
+-- PRIVATE FUNCTIONS
 
-function mail_parser.from_charset_to_utf8(charset, text)
-	-- TODO: move `gsub("-", "_"):upper()` into convert_charsets.normalize_charset_name
-	local to_utf8_mapping_table = convert_charsets.get_mapping_table_to_utf8(convert_charsets.normalize_charset_name(charset:gsub("-", "_"):upper()))
-	return convert_charsets.to_utf8(text, to_utf8_mapping_table)
+local IteratorStack = {}
+IteratorStack.__index = IteratorStack
+
+function IteratorStack:create(iterator)
+	local new = {}
+	setmetatable(new, self)
+	new.iterator = iterator
+	new.stack = {}
+	return new
 end
 
-function mail_parser.quotation_decode(charset, quotation_type, data)
-	local decoded_data = (quotation_type == 'B' and mime.unb64(data)) or (quotation_type == 'Q' and mime.unqp(data)) or data
-	return mail_parser.from_charset_to_utf8(charset, decoded_data)
+function IteratorStack:next()
+	local value = table.remove(self.stack)
+	if not value then
+		value = self.iterator()
+	end
+	if (value:sub(-1) == '\x0D') then
+		value = value:sub(1, -2)
+	end
+	return value
 end
 
-function mail_parser.unquote(quoted_string)
-	return quoted_string:gsub('=%?([^?]+)%?(.)%?([^?]+)%?=', mail_parser.quotation_decode)
+function IteratorStack:values()
+	return function () return self:next() end
 end
 
-function mail_parser.extract_headers(lines, start_line_number, boundary)
-	local headers, end_line_number = {}, #lines + 1
-	local last_header = nil
-	for i = start_line_number or 1, #lines do
-		local line = lines[i]
-		-- exit on an empty line (the next line is the beginning of a body) or on a boundary (the boundary line needs to be processed so do not skip it)
-		if (line == "") then
-			end_line_number = i + 1
-			break
-		end
-		if boundary and ((line == '--' .. boundary) or (line == '--' .. boundary .. '--')) then
-			end_line_number = i
-			break
-		end
-		-- parse a new header
-		local _, _, header, value = line:find("^([^:%s]+):%s*(.*)$")
-		-- parse a continuos header
-		local try_continuous = not header
-		if try_continuous then
-			header = last_header
-			_, _, value = line:find("^%s+(.*)$")
-		end
-		-- process a continuos or new header
-		if value then
-			header = header:lower()
-			value = mail_parser.unquote(value)
-			headers[header] = headers[header] and (headers[header] .. (try_continuous and "" or "\n") .. value) or value
-			last_header = header
-		end
-	end
-	return headers, end_line_number
-end
-
-function mail_parser.extract_body(lines, start_line_number, boundary, encoding)
-	local body, end_line_number = "", #lines + 1
-	for i = start_line_number or 1, #lines do
-		local line = lines[i]
-		-- exit on a boundary
-		if boundary and ((line == '--' .. boundary) or (line == '--' .. boundary .. '--')) then
-			end_line_number = i
-			break
-		end
-		-- process a continuos or new line
-		if (encoding == 'quoted-printable') then
-			-- mime.unqp do no know how to decode and empty string, so it returns null
-			body = body .. ((line:sub(-1) == "=")
-				-- if it is continuous then add without the suffix and \n
-				and (mime.unqp(line:sub(1, -2)) or "")
-				-- if it is non-continuous then add with \n
-				or ((mime.unqp(line) or "") .. "\n"))
-		elseif (encoding == 'base64') then
-			-- base64 can be decoded by individual lines
-			body = body .. mime.unb64(line)
-		else
-			body = body .. line .. "\n"
-		end
-	end
-	return body, end_line_number
-end
-
-function mail_parser.parse_content_type(content_type)
-	-- missing the content type
-	if not content_type then
-		return nil, nil, nil
-	end
-	-- text/* in a charset
-	local _, _, text_type, charset = content_type:find('^text/([^;]+);%s*charset="?([^"]+)"?$')
-	if text_type then
-		return 'text', text_type, charset
-	end
-	local _, _, text_type = content_type:find('^text/([^;]+)$')
-	if text_type then
-		return 'text', text_type, nil
-	end
-	-- multipart/* with a boundary
-	local _, _, multipart, boundary = content_type:find('^multipart/([^;]+);%s*boundary="?([^"]+)"?$')
-	if multipart then
-		return 'multipart', multipart, boundary
-	end
-	-- other data with a name
-	local _, _, major_type, minor_type, name = content_type:find('^([^/]+)/([^;]+);.*name="?([^";]+)"?.-$')
-	if major_type then
-		return 'data', major_type .. '/' .. minor_type, name
-	end
-	local _, _, major_type, minor_type = content_type:find('^([^/]+)/([^;]+);?.-$')
-	if major_type then
-		return 'data', major_type .. '/' .. minor_type, nil
-	end
-	-- unknown (however, everything should be processed as the cases above)
-	return nil, nil, nil
-end
-
-function mail_parser.parse_parts(lines, start_line_number, boundary)
-	local parts, end_line_number = {}, #lines + 1
-	local i = start_line_number or 1
-	while (i <= #lines) do
-		-- start processing of a part on a boundary entry tag
-		if (lines[i] == '--' .. boundary) then
-			local parsed, sub_end_line_number = mail_parser.parse(lines, i + 1, boundary)
-			table.insert(parts, parsed)
-			i = sub_end_line_number
-		-- exit on a boundary exit tag
-		elseif (lines[i] == '--' .. boundary .. '--') then
-			end_line_number = i
-			break
-		else
-			i = i + 1
-		end
-	end
-	return parts, end_line_number
-end
-
-function mail_parser.parse(lines, start_line_number, boundary)
-	local content, end_line_number
-	-- get headers
-	content, end_line_number = mail_parser.extract_headers(lines, start_line_number, boundary)
-	-- check the content type
-	local detected_type, description, specification = mail_parser.parse_content_type(content["content-type"])
-	-- parts in the case of a multipart content (the specification is a boundary)
-	if (detected_type == 'multipart') then
-		content['parts'], end_line_number = mail_parser.parse_parts(lines, end_line_number, specification)
-	-- text body in a charset in the case of a text content (the specification is the charset)
-	elseif (detected_type == 'text') then
-		content['body'], end_line_number = mail_parser.extract_body(lines, end_line_number, boundary, content["content-transfer-encoding"])
-		content['body-type'] = 'text/' .. description
-		-- convert from the charset
-		if specification then
-			content['body'] = mail_parser.from_charset_to_utf8(specification, content['body'])
-			content['body-original-charset'] = specification
-		end
-	-- data body with a name (the specification is the name)
-	elseif (detected_type == 'data') then
-		content['body'], end_line_number = mail_parser.extract_body(lines, end_line_number, boundary, content["content-transfer-encoding"])
-		content['body-type'] = description
-		content['body-name'] = specification
-	end
-	return content, end_line_number
+function IteratorStack:insert(value)
+	table.insert(self.stack, value)
 end
 
 local function print_table(table_to_print, prefix)
@@ -203,25 +79,170 @@ local function export_parsed(parsed, directory)
 	file_out:close()
 end
 
+-- PUBLIC FUNCTIONS
+
+function _M.from_charset_to_utf8(charset, text)
+	-- TODO: move `gsub("-", "_"):upper()` into convert_charsets.normalize_charset_name
+	local to_utf8_mapping_table = convert_charsets.get_mapping_table_to_utf8(convert_charsets.normalize_charset_name(charset:gsub("-", "_"):upper()))
+	return convert_charsets.to_utf8(text, to_utf8_mapping_table)
+end
+
+function _M.quotation_decode(charset, quotation_type, data)
+	local decoded_data = (quotation_type == 'B' and mime.unb64(data)) or (quotation_type == 'Q' and mime.unqp(data)) or data
+	return _M.from_charset_to_utf8(charset, decoded_data)
+end
+
+function _M.unquote(quoted_string)
+	return quoted_string:gsub('=%?([^?]+)%?(.)%?([^?]+)%?=', _M.quotation_decode)
+end
+
+function _M.parse_content_type(content_type)
+	-- missing the content type
+	if not content_type then
+		return nil, nil, nil
+	end
+	-- text/* in a charset
+	local _, _, text_type, charset = content_type:find('^text/([^;]+);%s*charset="?([^"]+)"?$')
+	if text_type then
+		return 'text', text_type, charset
+	end
+	local _, _, text_type = content_type:find('^text/([^;]+)$')
+	if text_type then
+		return 'text', text_type, nil
+	end
+	-- multipart/* with a boundary
+	local _, _, multipart, boundary = content_type:find('^multipart/([^;]+);%s*boundary="?([^"]+)"?$')
+	if multipart then
+		return 'multipart', multipart, boundary
+	end
+	-- other data with a name
+	local _, _, major_type, minor_type, name = content_type:find('^([^/]+)/([^;]+);.*name="?([^";]+)"?.-$')
+	if major_type then
+		return 'data', major_type .. '/' .. minor_type, name
+	end
+	local _, _, major_type, minor_type = content_type:find('^([^/]+)/([^;]+);?.-$')
+	if major_type then
+		return 'data', major_type .. '/' .. minor_type, nil
+	end
+	-- unknown (however, everything should be processed as the cases above)
+	return nil, nil, nil
+end
+
+function _M.extract_headers(lines, boundary)
+	local headers = {}
+	local last_header = nil
+	for line in lines:values() do
+		-- exit on an empty line (the next line is the beginning of a body) or on a boundary (the boundary line needs to be processed so do not skip it)
+		if (line == "") then
+			break
+		end
+		if boundary and ((line == '--' .. boundary) or (line == '--' .. boundary .. '--')) then
+			lines:insert(line)
+			break
+		end
+		-- parse a new header
+		local _, _, header, value = line:find("^([^:%s]+):%s*(.*)$")
+		-- parse a continuos header
+		local try_continuous = not header
+		if try_continuous then
+			header = last_header
+			_, _, value = line:find("^%s+(.*)$")
+		end
+		-- process a continuos or new header
+		if value then
+			header = header:lower()
+			value = _M.unquote(value)
+			headers[header] = headers[header] and (headers[header] .. (try_continuous and "" or "\n") .. value) or value
+			last_header = header
+		end
+	end
+	return headers
+end
+
+function _M.extract_body(lines, boundary, encoding)
+	local body = ""
+	for line in lines:values() do
+		-- exit on a boundary (the boundary line needs to be processed so do not skip it)
+		if boundary and ((line == '--' .. boundary) or (line == '--' .. boundary .. '--')) then
+			lines:insert(line)
+			break
+		end
+		-- process a continuos or new line
+		if (encoding == 'quoted-printable') then
+			-- mime.unqp do no know how to decode and empty string, so it returns null
+			body = body .. ((line:sub(-1) == "=")
+				-- if it is continuous then add without the suffix and \n
+				and (mime.unqp(line:sub(1, -2)) or "")
+				-- if it is non-continuous then add with \n
+				or ((mime.unqp(line) or "") .. "\n"))
+		elseif (encoding == 'base64') then
+			-- base64 can be decoded by individual lines
+			body = body .. mime.unb64(line)
+		else
+			body = body .. line .. "\n"
+		end
+	end
+	return body
+end
+
+function _M.parse_parts(lines, boundary)
+	local parts = {}
+	for line in lines:values() do
+		-- start processing of a part on a boundary entry tag
+		if (line == '--' .. boundary) then
+			local parsed = _M.parse(lines, boundary)
+			table.insert(parts, parsed)
+		-- exit on a boundary exit tag (the boundary line can be consumed here, it wont be needed anymore)
+		elseif (line == '--' .. boundary .. '--') then
+			break
+		end
+	end
+	return parts
+end
+
+function _M.parse(lines, boundary)
+	-- get headers
+	local content = _M.extract_headers(lines, boundary)
+	-- check the content type
+	local detected_type, description, specification = _M.parse_content_type(content["content-type"])
+	-- parts in the case of a multipart content (the specification is a boundary)
+	if (detected_type == 'multipart') then
+		content['parts'] = _M.parse_parts(lines, specification)
+	-- text body in a charset in the case of a text content (the specification is the charset)
+	elseif (detected_type == 'text') then
+		content['body'] = _M.extract_body(lines, boundary, content["content-transfer-encoding"])
+		content['body-type'] = 'text/' .. description
+		-- convert from the charset
+		if specification then
+			content['body'] = _M.from_charset_to_utf8(specification, content['body'])
+			content['body-original-charset'] = specification
+		end
+	-- data body with a name (the specification is the name)
+	elseif (detected_type == 'data') then
+		content['body'] = _M.extract_body(lines, boundary, content["content-transfer-encoding"])
+		content['body-type'] = description
+		content['body-name'] = specification
+	end
+	return content
+end
+
 -- main method for CLI
-function mail_parser.main(arg)
+function _M.main(arg)
 	if (#arg == 0 or arg[#arg] == "--help") then
 		stderr:write("Usage: " .. arg[0] .. " <mail-message-file> <output-directory>\n")
 		stderr:write("Parse a give mail message file and extracts its content into a given output directory.\n")
 		return 1
 	end
 	local opt_input, out_directory = arg[1], arg[2]
-	-- read file
-	local file_in, lines = (opt_input == "-") and stdin or assert(open(opt_input, "r")), {}
-	for line in file_in:lines() do
-		table.insert(lines, (line:sub(-1) == '\x0D') and line:sub(1, -2) or line)
-	end
+	-- read and parse file
+	local file_in = (opt_input == "-") and stdin or assert(open(opt_input, "r"))
+	local linesIteratorStack = IteratorStack:create(file_in:lines())
+	local parsed = _M.parse(linesIteratorStack, nil)
 	file_in:close()
-	-- process file
-	local parsed, count = mail_parser.parse(lines, nil, nil)
+	-- print/export parsed
 	export_parsed(parsed, out_directory)
 	--print_table(parsed, "")
 	return 0
 end
 
-return mail_parser
+return _M
